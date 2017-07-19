e8c4c3f3b0b51b52f2eb4f39d0e896c0ee8b8c82
shiho_yuki <shiho_yuki@cyberagent.co.jp>
2017-07-19 19:29:03 +0900

    Remove unnecessary codes

--- useKodein/src/main/kotlin/com/shihochan/kotlindiexample/di/app/AppModule.kt
+++ useKodein/src/main/kotlin/com/shihochan/kotlindiexample/di/app/AppModule.kt
@@ -1,6 +1,5 @@
 package com.shihochan.kotlindiexample.di.app
 
-import android.app.Activity
 import android.app.Application
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.bind
@@ -13,24 +12,3 @@ import com.github.salomonbrys.kodein.instance
 fun providesApp(app: Application) = Kodein.Module {
   bind<Application>() with instance(app)
 }
-
-
-inline fun inlineFun(fn: () -> Unit) {
-  fn()
-}
-
-fun hoge() {
-  inlineFun { print("hoge") }
-}
-
-val pair1 = 1 to 2
-val pair2 = 1.to(2)
-
-fun getName(clazz: Class<*>): String = clazz.simpleName
-
-val name = getName(Activity::class.java)
-
-val namesss = getName2<Activity>()
-
-inline fun <reified T> getName2(): String = T::class.java.simpleName
-
